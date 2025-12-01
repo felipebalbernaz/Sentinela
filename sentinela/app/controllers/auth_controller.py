4e9dfba8724f7a8ffebb3df59cb34e398d1623e2
Henrique Galvão <henriquegalvao@discente.ufg.br>
2025-12-01 20:16:30 -0300

    clean up code

--- sentinela/app/controllers/auth_controller.py
+++ sentinela/app/controllers/auth_controller.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template
-
+# criando blueprints de registro e login
 auth_bp = Blueprint('auth', __name__)
-
 @auth_bp.route('/login')
 def login():
-    return render_template('index.html') # Reusing index as login for now
-
+    return render_template('index.html')
 @auth_bp.route('/register')
 def register():
     return render_template('register.html')
