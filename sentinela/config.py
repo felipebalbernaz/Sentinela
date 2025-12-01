140c7e0ef26cbb27acb5e908772e696357b80b04
Henrique Galvão <henriquegalvao@discente.ufg.br>
2025-12-01 20:12:44 -0300

    Refactor SECRET_KEY assignment in config.py

--- sentinela/config.py
+++ sentinela/config.py
@@ -1,6 +1,6 @@
 import os
-
+# importar as keys secretas
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///sentinela.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
